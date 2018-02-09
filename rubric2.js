86cda68b1255fc4112910f3aff6b0cd94d7f5efb
adrianalynn <35774920+adrianalynn@users.noreply.github.com>
2018-02-09 15:19:24 -0600

    Add files via upload

---
+++ rubric2.js
@@ -0,0 +1,40 @@
+// JavaScript Document
+function myGreeting(a,b) {
+	var theGreeting= (a +b);
+	alert(theGreeting)	
+}
+
+myGreeting ("Hello, ", "everyone");
+
+
+var showName= prompt ("What is your favorite tv show?");
+switch (showName) {
+	case "the simpsons":
+    alert ("A classic!");
+    break;
+    case "King of the Hill":
+    alert ("This is my favorite, too!");
+    break;
+    default: 
+    alert ("I've never watched that one! Tell me about it next time we see eachother!");
+	break;
+	   
+}
+
+
+var candyBarName= prompt ("What is your favorite candy bar?");
+switch (CandyBarName) {
+	case "twix":
+    alert ("Mine too!");
+    break;
+    case "reeces":
+    alert ("That's not a candy bar!");
+    break;
+    case "kit-kat":
+    alert ("Break me off a piece!");
+    break;
+    default: 
+    alert ("I've never tried that one!");
+	break;
+   
+}
